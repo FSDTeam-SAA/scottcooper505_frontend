d2de547e700321add142e3e71b09aed63eb8f143
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-10-19 11:35:54 +0600

    reset password done

---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ResetPasswordForm from "../_component/reset-password-form";
+
+const page = () => {
+  return (
+    <div className="container lg:max-w-5xl py-16 lg:py-0">
+      <ResetPasswordForm />
+    </div>
+  );
+};
+
+export default page;
