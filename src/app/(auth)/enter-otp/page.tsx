5efe5da5d9403d245284a94824b29c3793bf7823
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-10-19 11:28:22 +0600

    enter otp done

---
+++ src/app/(auth)/enter-otp/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EnterOtpForm from "../_component/enter-otp-form";
+
+const page = () => {
+  return (
+    <div className="container lg:max-w-5xl py-16 lg:py-0">
+      <EnterOtpForm />
+    </div>
+  );
+};
+
+export default page;
