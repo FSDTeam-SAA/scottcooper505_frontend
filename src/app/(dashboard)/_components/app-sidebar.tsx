d02e4276a0a8e00273da296ff7bfd5119ae293c2
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-10-23 09:16:47 +0600

    dashboard layout done

---
+++ src/app/(dashboard)/_components/app-sidebar.tsx
@@ -0,0 +1,105 @@
+"use client";
+import {
+  CookingPot,
+  HandPlatter,
+  LayoutDashboard,
+  LogOut,
+  Settings,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Services",
+    url: "/dashboard/services",
+    icon: HandPlatter,
+  },
+
+  {
+    title: "Booking History",
+    url: "/dashboard/booking-history",
+    icon: CookingPot,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  const pathName = usePathname();
+
+  return (
+    <Sidebar>
+      <SidebarContent className="py-10 lg:border-r lg:border-black/40">
+        <SidebarGroup className="h-full">
+          <SidebarGroupLabel className="h-[60px] w-[113px] mx-auto">
+            {/* logo */}
+            <Link href={"/"}>
+              <div className="flex justify-center w-full mb-10">
+                <Image
+                  src={"/logo.png"}
+                  alt="logo.png"
+                  width={1000}
+                  height={1000}
+                  className="h-[60px] w-[113px] mx-auto"
+                />
+              </div>
+            </Link>
+          </SidebarGroupLabel>
+
+          <SidebarGroupContent className="h-full flex flex-col justify-between">
+            <SidebarMenu>
+              {items.map((item) => {
+                const isActive = pathName === item.url;
+
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      className={`font-medium hover:bg-primary hover:text-white ${
+                        isActive && "bg-primary text-white"
+                      } transition-all duration-200`}
+                      asChild
+                    >
+                      <Link href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+
+            <SidebarFooter className="border-t border-gray-300">
+              <button className="font-medium text-red-500 flex items-center gap-2">
+                <LogOut className="h-4 w-4" /> Log out
+              </button>
+            </SidebarFooter>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
