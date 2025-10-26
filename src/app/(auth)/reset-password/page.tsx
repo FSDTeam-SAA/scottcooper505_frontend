3a854628828ea8b8f55bc63882b849b9326b7187
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-10-26 17:53:01 +0600

    confilct solve

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ResetPasswordForm from "../_component/reset-password-form";
 
 const page = () => {
   return (
     <div className="container lg:max-w-5xl py-16 lg:py-0">
-      <ResetPasswordForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
     </div>
   );
 };
